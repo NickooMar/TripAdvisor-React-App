6f4fc8d71cc4793e54ecf8de5e7af79ec39d7ac2
Nicolás <nicoo.marsili@gmail.com>
2022-02-14 21:00:07 -0300

    Weather feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-function App() {
+import { getPlacesData, getWeatherData } from "./api";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+const App = () => {
+
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+      setCoordinates({lat: latitude, lng: longitude});
+    })
+  }, []);
+  
+  useEffect(() => {  //Rating useEffect
+    const filteredPlaces = places.filter((place) => place.rating > rating); 
+    setFilteredPlaces(filteredPlaces);
+  }, [rating])
+  
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+
+      getWeatherData(coordinates.lat, coordinates.lng)
+        .then((data) => setWeatherData(data));
+
+      getPlacesData(type, bounds.sw, bounds.ne)
+        .then((data) => {
+          setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+          setFilteredPlaces([]) //Every time the data is updated, the filteredPlaces are reset to an empty array
+          setIsLoading(false);
+      })
+    }
+  }, [type, bounds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>{/*This xs take full width on mobile devices, in medium or larger devices take 4 spaces.*/}
+          <List 
+            places={filteredPlaces.length ? filteredPlaces : places} 
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map 
+            places={filteredPlaces.length ? filteredPlaces : places} 
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            setChildClicked={setChildClicked}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating"; // It's on beta
+
+import useStyles from "./styles";
+import { mapStyles } from './mapStyles';
+
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData }) => {
+  
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{disableDefaultUI: true, zoomControl: true, styles: mapStyles}}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)} //Use when you click a restaurant on the map.
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {isMobile ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography className={classes.typography} variant="subtitle2" gutterBottom >{place.name}</Typography>
+                <img className={classes.pointer} 
+                  src={ place.photo ? place.photo.images.large.url : "https://media-cdn.tripadvisor.com/media/photo-s/17/75/d7/11/rush-bar.jpg" } 
+                  alt={place.name}
+                  />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+
+          {weatherData?.list?.map((data, i) => (
+            <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+              <img height='100' src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} />
+            </div>
+          ))}
+
+
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
